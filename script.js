9aaeeb05a4c530b405c7ff51fca43e961522fe1b
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2022-05-12 15:05:53 +0200

    Refacto

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.querySelector("p").addEventListener("click", function(){
-    document.querySelector("p").classList.toggle("blue");
+const MENU = document.querySelectorAll(".menu li");
+const CARDS = document.querySelectorAll(".cardContent");
+
+MENU.forEach(menuItem =>{
+    menuItem.addEventListener("mouseover", function(){
+        menuItem.style.backgroundColor="rgb(66, 75, 80)";
+        menuItem.firstElementChild.setAttribute("src", `${menuItem.getAttribute("data-name")}white.png`);
+        menuItem.addEventListener("mouseout", function(){
+            menuItem.style.backgroundColor="transparent";
+            menuItem.firstElementChild.setAttribute("src", `${menuItem.getAttribute("data-name")}.png`);
+        })
+
+    })
+}) 
+
+CARDS.forEach(card =>{
+    card.addEventListener("mouseover", function(){
+        card.style.position
+    })
 })
+
